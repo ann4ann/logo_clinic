94b2160e17079f091691b0194ab5d76428e04cdc
Anna Noskova <anni4anny@gmail.com>
2023-07-30 17:17:44 +0300

    initial commit

--- dbConfig.php
+++ dbConfig.php
@@ -5,11 +5,6 @@ define('DBNAME','logo_clinic');
 define('DBUSER','root');
 define('DBPWD','');
 
-// БД
-// define('DBHOST','localhost');
-// define('DBNAME','d90618j6_logo');
-// define('DBUSER','d90618j6_logo');
-// define('DBPWD','8*rsC*PK');
 
 $dbhost = DBHOST;
 $dbname = DBNAME;
